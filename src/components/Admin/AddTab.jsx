ab519070a038b63bbde0843bf1ddd9b042165b02
CarePerro0926 <programacion0926@gmail.com>
2025-10-26 10:35:14 -0500

    addd

--- src/components/Admin/AddTab.jsx
+++ src/components/Admin/AddTab.jsx
@@ -42,7 +42,7 @@ const AddTab = ({
     const { id, nombre, categoria_id, cantidad, precio, provider_id } = nuevoProducto;
 
     if (!id || !nombre || !categoria_id || !cantidad || !precio || !provider_id) {
-      alert('Por favor completa todos los campos del producto.');
+      alert('Completa todos los campos del producto.');
       return;
     }
 
@@ -139,7 +139,6 @@ const AddTab = ({
             required
           >
             <option value="">Seleccionar categoría</option>
-
             {categorias.map((cat) => (
               <option key={cat.id} value={cat.id}>
                 {cat.id} — {cat.nombre}
@@ -181,10 +180,9 @@ const AddTab = ({
             required
           >
             <option value="">Seleccionar proveedor</option>
-
             {proveedores.map((prov) => (
               <option key={prov.id} value={prov.id}>
-                {prov.nombre}
+                {prov.id} — {prov.nombre}
               </option>
             ))}
           </select>
