f5290e7f64326a5412a438b71ac8dc370f451e11
CarePerro0926 <programacion0926@gmail.com>
2025-10-27 17:16:52 -0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+console.log("main.jsx: Iniciando la aplicación"); // <-- Nuevo log
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
