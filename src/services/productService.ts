20cba1269437746e23f57ac98b5117a5e2d7c2d0
CarePerro0926 <programacion0926@gmail.com>
2025-10-26 22:21:51 -0500

    act

---
+++ src/services/productService.ts
@@ -0,0 +1,11 @@
+import { supabase } from '../supabaseClient';
+
+export const getAll = async () => {
+  const { data, error } = await supabase
+    .from('vista_productos_con_categoria')
+    .select('*');
+
+  if (error) throw error;
+
+  return data;
+};
