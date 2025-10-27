9071418ada61d78032defd2ffb12913187f15007
CarePerro0926 <programacion0926@gmail.com>
2025-10-27 17:34:52 -0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import App from './App.jsx'; // <-- Comentado temporalmente
+// import './index.css'; // <-- Opcionalmente comentado
+// import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Opcionalmente comentado
 
 console.log("main.jsx: Iniciando la aplicación"); // <-- Nuevo log
 
+// Renderiza un componente vacío para ver si el log aparece
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>Hola Mundo</div>
 );
