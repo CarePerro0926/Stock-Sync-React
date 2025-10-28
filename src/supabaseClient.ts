33fc5b0bcc9dc867ae11d80e777afc384fd7f7bb
CarePerro0926 <programacion0926@gmail.com>
2025-10-28 14:18:14 -0500

    supabase

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://vdprbdudjoqiptxavzmt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkcHJiZHVkam9xaXB0eGF2em10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNzM5NDQsImV4cCI6MjA3Njc0OTk0NH0.9sogcsR0gTaTe6nRsrsDxn8juj_atjw0PSxNWG-TAUA'
-);
