0158c96704b769c6f0af14c493e32b3ae4b68e7e
CarePerro0926 <programacion0926@gmail.com>
2025-10-28 12:39:42 -0500

    supabase

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vdprbdudjoqiptxavzmt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkcHJiZHVkam9xaXB0eGF2em10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNzM5NDQsImV4cCI6MjA3Njc0OTk0NH0.9sogcsR0gTaTe6nRsrsDxn8juj_atjw0PSxNWG-TAUA';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase environment variables are missing');
+}
 
 const supabase = createClient(supabaseUrl.trim(), supabaseKey);
 
