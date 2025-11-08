ac8f599482db37b3546b205d13afcbcd5307c5f5
CarePerro0926 <programacion0926@gmail.com>
2025-11-08 18:28:31 -0500

    UsuariosView Test responsive

--- src/components/UsuariosView.jsx
+++ src/components/UsuariosView.jsx
@@ -104,7 +104,7 @@ const UsuariosView = () => {
   ];
 
   return (
-    <div className="container-fluid position-relative" style={{ minHeight: '100vh' }}>
+    <div className="container-fluid position-relative">
       {/* Botón fijo fuera del scroll */}
       <div className="position-fixed top-0 end-0 m-3 z-3">
         <button className="btn btn-danger" onClick={cerrarSesion}>
@@ -117,7 +117,7 @@ const UsuariosView = () => {
 
         {/* Filtros estilo UpdateTab */}
         <div className="row g-2 mb-3">
-          <div className="col-12 col-md-6">
+          <div className="col-12 col-md">
             <select
               className="form-select"
               value={filtroRol}
@@ -130,7 +130,7 @@ const UsuariosView = () => {
               ))}
             </select>
           </div>
-          <div className="col-12 col-md-6">
+          <div className="col-12 col-md">
             <input
               className="form-control"
               placeholder="Buscar por nombre, apellido, email o usuario..."
