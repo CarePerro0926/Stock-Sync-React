8da37c2b497675af632cd2d08b197d6ae3c6b754
CarePerro0926 <programacion0926@gmail.com>
2025-11-08 21:41:23 -0500

    fix responsive tabla usuarios

--- src/components/UsuariosView.jsx
+++ src/components/UsuariosView.jsx
@@ -107,7 +107,7 @@ const UsuariosView = () => {
 
       {/* Tarjetas con scroll interno */}
       {!loading && (
-        <div className="flex-grow-1 overflow-auto" style={{ maxHeight: '50vh', paddingRight: '8px' }}>
+        <div style={{ height: '70vh', overflowY: 'auto', paddingRight: '8px' }}>
           {usuariosFiltrados.length === 0 ? (
             <div className="text-center p-4">
               <p>No se encontraron usuarios que coincidan con los filtros.</p>
